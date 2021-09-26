31d834965a064efa2f8fa4cc78d1c00ac101e46c
Narayanan PP <pnkrishnan27@gmail.com>
2021-09-26 18:32:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jnocr_client',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/narayananpp/jnocr_client',
+    license='Apache',
+    author='Narayanan PP',
+    author_email='pnkrishnan27@gmail.com',
+    description='jnocr_client'
+)

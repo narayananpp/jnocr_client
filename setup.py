51de736cb58995925e49c292d309db72110695c1
Narayanan PP <pnkrishnan27@gmail.com>
2021-09-26 18:02:38 +0530

    commit LICENSE, setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ocr_client',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/narayananpp/jnocr_client.git',
+    license='Apache',
+    author='Narayanan PP',
+    author_email='pnkrishnan27@gmail.com',
+    description='Python client for jnocr'
+)
